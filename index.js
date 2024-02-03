d8213aacacacc21ee19b9d555ebd8c7fe29be7c9
Marmik soni <marmiksoni1108@gmail.com>
2024-02-03 19:50:51 +0530

    Toke try

--- index.js
+++ index.js
@@ -3,10 +3,7 @@ let hello2 = 'marmik2';
 let hello3 = 'marmik3';
 const num = 23;
 const num2 = 233;
-console.log("Removed rules for all")
-console.log("Removed rules for id same marmiksoni11")
 
-console.log("dsdsdsdsdsd");
-console.log("sds");
-console.log("hedsdsddsdllo");
-console.log("hedsdsllo");
+
+
+// now this is the  lastest try for token settings .....
